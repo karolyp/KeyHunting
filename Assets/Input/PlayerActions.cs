edaa26508499136d1ae569818383082a1276fa7a
Károly Pákozdi <karoly.pakozdi.150@gmail.com>
2021-05-26 14:24:17 +0200

    Initial commit

---
+++ Assets/Input/PlayerActions.cs
@@ -0,0 +1,205 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/PlayerActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerActions"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerControls"",
+            ""id"": ""fc93e601-2ef5-40e1-8c47-ef411b4f1e44"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""fa099b92-f247-4283-a2bc-cdffb58a0dc8"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""MouseMovemnt"",
+                    ""type"": ""Value"",
+                    ""id"": ""39b033f7-3213-41f6-a90c-b5543445c03e"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""8a315170-aefa-4e72-a7f3-a7074d6b1d76"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""1dfa6112-7081-4a71-afc9-54996a13df57"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""8139a8dc-15fd-4506-9e5f-6bec16851e87"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""58035461-b482-4ce6-bb0d-a2589634616a"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""19cfe329-782c-416d-b72b-9f9ea231dc0d"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""77ef2c8f-e745-4e3d-8865-34146621cbde"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MouseMovemnt"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerControls
+        m_PlayerControls = asset.FindActionMap("PlayerControls", throwIfNotFound: true);
+        m_PlayerControls_Movement = m_PlayerControls.FindAction("Movement", throwIfNotFound: true);
+        m_PlayerControls_MouseMovemnt = m_PlayerControls.FindAction("MouseMovemnt", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerControls
+    private readonly InputActionMap m_PlayerControls;
+    private IPlayerControlsActions m_PlayerControlsActionsCallbackInterface;
+    private readonly InputAction m_PlayerControls_Movement;
+    private readonly InputAction m_PlayerControls_MouseMovemnt;
+    public struct PlayerControlsActions
+    {
+        private @PlayerActions m_Wrapper;
+        public PlayerControlsActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_PlayerControls_Movement;
+        public InputAction @MouseMovemnt => m_Wrapper.m_PlayerControls_MouseMovemnt;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerControlsActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerControlsActions instance)
+        {
+            if (m_Wrapper.m_PlayerControlsActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMovement;
+                @MouseMovemnt.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMouseMovemnt;
+                @MouseMovemnt.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMouseMovemnt;
+                @MouseMovemnt.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMouseMovemnt;
+            }
+            m_Wrapper.m_PlayerControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @MouseMovemnt.started += instance.OnMouseMovemnt;
+                @MouseMovemnt.performed += instance.OnMouseMovemnt;
+                @MouseMovemnt.canceled += instance.OnMouseMovemnt;
+            }
+        }
+    }
+    public PlayerControlsActions @PlayerControls => new PlayerControlsActions(this);
+    public interface IPlayerControlsActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnMouseMovemnt(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 12f;
+    public float sprintSpeed = 24f;
+    public float mouseSensitivity = 30f;
+    public float jumpHeight = 3f;
+    public float gravity = 19.62f;
+    public Camera attachedCamera;
+    public LayerMask groundMask;
+    public Transform groundCheck;
+    public TextMeshProUGUI counter;
+    public TextMeshProUGUI endOfGame;
+
+    private Vector2 movement;
+    private Vector2 mouseMovement;
+    private CharacterController controller;
+    private float xRotation = 0f;
+    private bool isSprinting = false;
+    private bool isJumping = false;
+    private Vector3 verticalVelocity = Vector3.zero;
+    private bool isGrounded;
+    private float numberOfKeys = 0f;
+    private float numberOfCollectedKeys = 0f;
+
+    private void Awake()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Start()
+    {
+        numberOfKeys = GameObject.FindGameObjectsWithTag("key").Length;
+        endOfGame.gameObject.SetActive(false);
+        PrintKeysCounter();
+    }
+
+    void Update()
+    {
+        float newSpeed = CalculateSpeed();
+        Vector3 moveVector = (transform.right * movement.x + transform.forward * movement.y) * newSpeed * Time.deltaTime;
+        controller.Move(moveVector);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
+
+        if (isGrounded)
+        {
+            verticalVelocity.y = 0f;
+            if (isJumping)
+            {
+                verticalVelocity.y = Mathf.Sqrt(jumpHeight * 2f * gravity);
+                controller.Move(verticalVelocity * Time.deltaTime);
+            }
+        }
+        else
+        {
+            verticalVelocity.y -= gravity * Time.deltaTime;
+            controller.Move(verticalVelocity * Time.deltaTime);
+        }
+
+
+        Look();
+        OnEscape();
+    }
+
+    private void Look()
+    {
+        mouseMovement *= mouseSensitivity * Time.deltaTime;
+        transform.Rotate(Vector3.up, mouseMovement.x);
+
+        xRotation -= mouseMovement.y;
+        xRotation = Mathf.Clamp(xRotation, -85f, 85f);
+
+        attachedCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    public void OnMovement(InputAction.CallbackContext ctx)
+    {
+        movement = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnMouseMovement(InputAction.CallbackContext ctx)
+    {
+        mouseMovement = ctx.ReadValue<Vector2>();
+    }
+
+    private void OnEscape()
+    {
+        // itt valamiért nem tudtam megfelelő actiont kiváltani az input systemben
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+
+    public void OnSprint(InputAction.CallbackContext ctx)
+    {
+        isSprinting = !isSprinting;
+        Debug.Log(isSprinting);
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        isJumping = ctx.performed;
+    }
+
+    private float CalculateSpeed()
+    {
+        if (isSprinting && isGrounded)
+        {
+            return sprintSpeed;
+        }
+        return speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "key")
+        {
+            numberOfCollectedKeys++;
+            PrintKeysCounter();
+            other.gameObject.SetActive(false);
+            if(numberOfCollectedKeys == numberOfKeys)
+            {
+                endOfGame.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private void PrintKeysCounter()
+    {
+        counter.text = $"Begyűjtött kulcsok: {numberOfCollectedKeys}/{numberOfKeys}";
+    }
+}
